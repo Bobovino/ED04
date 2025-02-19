76c090dc7a0725f585686e3b67ed38d90184ba27
Bobovino <rodrigomartineztabernero97@gmail.com>
2025-02-19 20:19:12 +0100

    Pasos 1,2 y 3

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # ED04
+
+REFACTORIZACIÓN
+
+1)Las clases deberán formar parte del paquete cuentas.
+2)Cambiar el nombre de la variable "miCuenta" por "cuenta1".
+3)Introducir el método operativa_cuenta, que englobe las sentencias de la clase Main que operan con el objeto cuenta1.
+4)Encapsular los atributos de la clase CCuenta.
+5)Añadir un nuevo parámetro al método operativa_cuenta, de nombre cantidad y de tipo float.
+
+GIT
+
+1)Configurar GIT para el proyecto. 
+2)Crear un repositorio público en GitHub.
+3)Realizar, al menos, una operación commit. Comentando el resultado de la ejecución.
+4)Mostrar el historial de versiones para el proyecto mediante un comando desde consola.
+
+JAVADOC
+
+1)Insertar comentarios JavaDoc en la clase CCuenta.
+2)Generar documentación JavaDoc para todo el proyecto y comprueba que abarca todos los métodos y atributos de la clase CCuenta.
--- cuentas/Main.java
+++ cuentas/Main.java
@@ -1,23 +1,29 @@
+//Paso 1, englobar en un paquete cuentas las clases CCuenta y Main
 package cuentas;
 
 public class Main {
-
     public static void main(String[] args) {
-        CCuenta miCuenta;
+        //En el paso 3 hemos refactorizado el método main para que llame a operativa_cuenta
+        operativa_cuenta();
+    }
+
+    public static void operativa_cuenta(){
+        //En el paso 2 hemos renombrado la variable  a cuenta1
+        CCuenta cuenta1;
         double saldoActual;
 
-        miCuenta = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
-        saldoActual = miCuenta.estado();
+        cuenta1 = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
+        saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es" + saldoActual);
 
         try {
-            miCuenta.retirar(2300);
+            cuenta1.retirar(2300);
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
         }
         try {
             System.out.println("Ingreso en cuenta");
-            miCuenta.ingresar(695);
+            cuenta1.ingresar(695);
         } catch (Exception e) {
             System.out.print("Fallo al ingresar");
         }
